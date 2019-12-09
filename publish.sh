193c8887aca35e86322f64aef0772c5720c1f449
Jon <xuchaoo@gmail.com>
2019-12-09 18:13:21 +0800

    update source

--- publish.sh
+++ publish.sh
@@ -30,7 +30,7 @@ then
 	tree
 
 	# 更新build 时间
-	sed  -i "s/(__build_time__)/`date`/" index.html
+	sed  -i "s/(__build_time__)/`date '+%Y-%m-%d %H:%M:%S'`/" index.html
 	git add .
 	git commit -m "Update blog"
 	git push -f "https://${GITHUB_TK}@github.com/jscrapy/jscrapy.github.io.git" master
